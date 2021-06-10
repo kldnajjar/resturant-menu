eafd155637f240c8f4c3b15d493f9eae35656e60
Khaled AlNajjar <khaled@homie.rent>
2021-06-11 02:45:42 +0300

    Adding missing lib

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./css/general.scss";
+
+function App() {
+  return (
+    <div className="main-container">
+      <iframe
+        loading="lazy"
+        className="iframe-container"
+        title="DAEgRruUTZg"
+        src="https://www.canva.com/design/DAEgRruUTZg/view?embed"
+      ></iframe>
+    </div>
+  );
+}
+
+export default App;
